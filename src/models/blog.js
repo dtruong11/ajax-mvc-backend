d0653ea979928b4609d108b62fc7fd8de2de65a0
dtruong11 <dtruong11@users.noreply.github.com>
2018-07-09 22:46:54 -0700

    Took out fs

--- src/models/blog.js
+++ src/models/blog.js
@@ -1,6 +1,8 @@
-const fs = require('fs')
-const path = require('path')
-const file = path.join(__dirname, './data.json')
+// const fs = require('fs')
+// const path = require('path')
+// const file = path.join(__dirname, './data.json')
+
+const data = require('./data')
 
 // Validation functions
 const validateBody = require('./validate').validateBody
@@ -8,7 +10,7 @@ const checkPost = require('./validate').checkPost
 
 // CRUD
 function getAll(limit) {
-  const posts = JSON.parse(fs.readFileSync(file, 'utf-8'))
+  const posts = data
   return limit ? posts.slice(0, limit) : posts
 }
 
@@ -17,7 +19,7 @@ function getOne(id) {
   if (errors.length > 0) return {
     errors
   }
-  const posts = JSON.parse(fs.readFileSync(file, 'utf-8'))
+  const posts = data
   return posts.find(el => el.id === id)
 }
 
@@ -36,12 +38,13 @@ function create(body) {
     content
   }
   posts.push(post)
-  fs.writeFileSync(file, JSON.stringify(posts))
+  // fs.writeFileSync(file, JSON.stringify(posts))
   return post
 }
 
 function update(id, body) {
-  const posts = JSON.parse(fs.readFileSync(file, 'utf-8'))
+  // const posts = JSON.parse(fs.readFileSync(file, 'utf-8'))
+  const posts = data
   const errors = [...checkPost(id), ...validateBody(body)]
   if (errors.length > 0) {
     return {
@@ -53,13 +56,11 @@ function update(id, body) {
   const content = body.content
   post.title = title
   post.content = content
-  console.log(id)
-  fs.writeFileSync(file, JSON.stringify(posts))
   return post
 }
 
 function destroy(id) {
-  const posts = JSON.parse(fs.readFileSync(file, 'utf-8'))
+  const posts = data
   const post = getOne(id)
   const errors = checkPost(id)
   if (errors.length > 0) {
@@ -68,7 +69,6 @@ function destroy(id) {
     }
   }
   const removed = posts.splice(posts.findIndex(el => el.id === id), 1)
-  fs.writeFileSync(file, JSON.stringify(posts))
   return removed
 }
 
